52fe4b058e2b2313b44f477a492cb48c2817c832
Trui-git <tengrui13@gmail.com>
2020-08-16 13:06:50 -0400

    modify padding and arrange lines

--- components/pages/AboutScreen.js
+++ components/pages/AboutScreen.js
@@ -10,24 +10,21 @@ const AboutScreen = () => {
             justifyContent: 'center',
             alignItems: 'center',
             backgroundColor: 'skyblue',
-            
-             }}>
-         
+            padding: 30
+        }}>        
     
-      <Text>SMRT-Stock Market Real-time !A simple app to display the stock information.
+        <Text>
+            SMRT-Stock Market Real-time !A simple app to display the stock information.
+            SMRT  app will show stock information ,what stock markets are there ,what are the top 10 stock markets?
+            will also display information about current and what are the different rules in stock market.will also
+            show comparison or graph about the top 10 markets.
+        </Text>
 
- 
-
-
-SMRT  app will show stock information ,what stock markets are there ,what are the top 10 stock markets?
-will also display information about current and what are the different rules in stock market.will also
-show comparison or graph about the top 10 markets.</Text>
-
-<Text>
-    Thankyou for choosing this app.We appreciate your comments always,please send the feature request or 
-    request or report bugsdirectly to the developer instead od posting on the market.we will do our best to satisfy
-    your request.
-</Text>
+        <Text>
+            Thankyou for choosing this app.We appreciate your comments always,please send the feature request or 
+            request or report bugsdirectly to the developer instead od posting on the market.we will do our best to satisfy
+            your request.
+        </Text>
     </View>
   );
 }
